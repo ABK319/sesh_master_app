c1739e3f155b8951f568ac22ad800a7f9160a8b6
Kieran Watson <2318086w@student.gla.ac.uk>
2019-02-28 18:01:53 +0000

    third commit: basic html skeleton files and urls paths

--- seshmaster/urls.py
+++ seshmaster/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
-from seshmaster import views
+from rango import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about', views.about, name='about'),
-    url(r'^sitemap', views.site_map, name='site_map'),
-    url(r'^contact', views.contact, name='contact'),
+url(r'^$', views.index, name='index'),
+url(r'contactus/', views.contactus, name='contactus'),
+url(r'signup/', views.signup, name='signup'),
+url(r'login/', views.login, name='login'),
+url(r'nightclubbrowse/', views.nightclubbrowse, name='nightclubbrowse'),
+url(r'nightclubbrowse/nightclubpage/', views.nightclubpage, name='nightclubpage'),	
+url(r'nightclubbrowse/leavereview/', views.leavereview, name='leavereview'),	
+url(r'nightclubbrowse/rateimage/', views.rateimage, name='rateimage'),			
+#url(r'/', views., name=''),						#template line
 ]
+
--- seshmaster/views.py
+++ seshmaster/views.py
@@ -1,21 +1,69 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def index(request):
-
-    context_dict = {'boldmessage': "hey!"}
-
-    return render(request,'seshmaster/index.html', context=context_dict)
+def home(request):
 
 
-def about(request):
-    return HttpResponse("About Us<a href='/seshmaster/'>Home</a>")
-
-def site_map(request):
-    return HttpResponse("Sitemap<a href='/seshmaster/'>Home</a>")
+	return render(request, 'seshmaster/home.html', context=context_dict)
 
 def contact(request):
-    return HttpResponse("contact info<a href='/seshmaster/'>Home</a>")
+	
+	
+	return render(request, 'seshmaster/contact.html', context=context_dict)
+	
+	
+def signup(request):
+	
+	
+	return render(request, 'seshmaster/signup.html', context=context_dict)
+	
+	
+def login(request):
+	
+	
+	return render(request, 'seshmaster/login.html', context=context_dict)
+	
+	
+def nightclubbrowse(request):
+	
+	
+	return render(request, 'seshmaster/nightclubbrowse.html', context=context_dict)
 
+	
+def nightclubpage(request):
+	
+	
+	return render(request, 'seshmaster/nightclubbrowse/nightclubpage.html', context=context_dict)
+	
 
+def leavereview(request):
+	
+	
+	return render(request, 'seshmaster/nightclubbrowse/leavereview.html', context=context_dict)
+	
+
+def rateimage(request):
+	
+	
+	return render(request, 'seshmaster/nightclubbrowse/rateimage.html', context=context_dict)
+	
+
+def myaccount(request):
+	
+	
+	return render(request, 'seshmaster/myaccount.html', context=context_dict)
+	
+	
+def myreviews(request):
+	
+	
+	return render(request, 'seshmaster/myaccount/myreviews.html', context=context_dict)
+	
+	
+def mynightclubs(request):
+	
+	
+	return render(request, 'seshmaster/mynightclubs.html', context=context_dict)
+	
+	
+	
