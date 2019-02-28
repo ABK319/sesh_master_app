21150d124a95ef781396ecee780950b109e39f04
Abhishek Kulkarni <2253426K@student.gla.ac.uk>
2019-02-28 18:16:29 +0000

    Two projects syncronized

--- sesh_master_app/urls.py
+++ sesh_master_app/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import include
-from rango import views
+from seshmaster import views
 from django.conf import settings
 
 urlpatterns = [
--- seshmaster/views.py
+++ seshmaster/views.py
@@ -1,69 +1,69 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
+def index(request):
 
 
-	return render(request, 'seshmaster/home.html', context=context_dict)
+	return render(request, 'seshmaster/index.html')
 
 def contact(request):
 	
 	
-	return render(request, 'seshmaster/contact.html', context=context_dict)
+	return render(request, 'seshmaster/contact.html')
 	
 	
 def signup(request):
 	
 	
-	return render(request, 'seshmaster/signup.html', context=context_dict)
+	return render(request, 'seshmaster/signup.html')
 	
 	
 def login(request):
 	
 	
-	return render(request, 'seshmaster/login.html', context=context_dict)
+	return render(request, 'seshmaster/login.html')
 	
 	
 def nightclubbrowse(request):
 	
 	
-	return render(request, 'seshmaster/nightclubbrowse.html', context=context_dict)
+	return render(request, 'seshmaster/nightclubbrowse.html')
 
 	
 def nightclubpage(request):
 	
 	
-	return render(request, 'seshmaster/nightclubbrowse/nightclubpage.html', context=context_dict)
+	return render(request, 'seshmaster/nightclubbrowse/nightclubpage.html')
 	
 
 def leavereview(request):
 	
 	
-	return render(request, 'seshmaster/nightclubbrowse/leavereview.html', context=context_dict)
+	return render(request, 'seshmaster/nightclubbrowse/leavereview.html')
 	
 
 def rateimage(request):
 	
 	
-	return render(request, 'seshmaster/nightclubbrowse/rateimage.html', context=context_dict)
+	return render(request, 'seshmaster/nightclubbrowse/rateimage.html')
 	
 
 def myaccount(request):
 	
 	
-	return render(request, 'seshmaster/myaccount.html', context=context_dict)
+	return render(request, 'seshmaster/myaccount.html')
 	
 	
 def myreviews(request):
 	
 	
-	return render(request, 'seshmaster/myaccount/myreviews.html', context=context_dict)
+	return render(request, 'seshmaster/myaccount/myreviews.html')
 	
 	
 def mynightclubs(request):
 	
 	
-	return render(request, 'seshmaster/mynightclubs.html', context=context_dict)
+	return render(request, 'seshmaster/mynightclubs.html')
 	
 	
 	
