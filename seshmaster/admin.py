2ab2ace8fe22bac3cf3e48d3079b18149609242f
Abhishek Kulkarni <2253426K@student.gla.ac.uk>
2019-03-07 16:52:14 +0000

    Slight changes to database made

--- seshmaster/admin.py
+++ seshmaster/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from seshmaster.models import User, Image, Nightclub,Review
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Image)
+admin.site.register(Nightclub)
+admin.site.register(Review)
--- seshmaster/models.py
+++ seshmaster/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    name = models.CharField(max_length=128, unique=True)
+    email = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+
+    
+    user = models.ForeignKey(User) 
+    
+    def __str__(self):
+        return self.score
+
+class Nightclub(models.Model):
+
+    image = models.ForeignKey(Image)
+    music_genre = models.CharField(max_length=100)
+    price = models.CharField(max_length=50)
+    description = models.CharField(max_length=400)
+    location = models.CharField(max_length=400)
+    average_score=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.score
+
+class Review(models.Model):
+
+    nightclub = models.ForeignKey(Nightclub)
+    user = models.ForeignKey(User) 
+    score = models.CharField(max_length=10)
+    Comment = models.CharField(max_length=350)
+
+    def __str__(self):
+        return self.score
+
+
+
+
